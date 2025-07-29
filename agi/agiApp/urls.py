4cc4dc5d9af7b6d8ee17863ec017f65d74ba7430
rara0316 <duf1qudtls2@gmail.com>
2025-07-29 10:02:49 +0900

    reset

--- agi/agiApp/urls.py
+++ agi/agiApp/urls.py
@@ -4,10 +4,4 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('api/analyze-query/', views.analyze_query, name='analyze_query'),
-    path('api/workflow/<str:workflow_id>/status/', views.workflow_status, name='workflow_status'),
-    path('api/alerts/recent/', views.get_recent_alerts, name='get_recent_alerts'),
-    path('api/alerts/<int:alert_id>/acknowledge/', views.acknowledge_alert, name='acknowledge_alert'),
-    path('api/monitoring/start/', views.start_monitoring, name='start_monitoring'),
-    path('api/monitoring/stop/', views.stop_monitoring, name='stop_monitoring'),
 ]
