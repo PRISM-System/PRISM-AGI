f5c97b89d606f1263c95e2a3b0a60f5c3ab2a7ab
rara0316 <duf1qudtls2@gmail.com>
2025-08-14 18:17:27 +0900

    에이전트 목록 추가

--- agi/agiApp/urls.py
+++ agi/agiApp/urls.py
@@ -6,9 +6,11 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('create-agent/', views.create_agent_page, name='create_agent'),
     path('manage-agents/', views.manage_agents_page, name='manage_agents'),
+    path('password-reset/', views.password_reset_view, name='password_reset'),
+    path('check-email/', views.check_email_api, name='check_email_api'),
+
+    # 로그인 및 회원가입
     path('login/', views.login_view, name='login'),
     path('register/', views.register_view, name='register'),
     path('logout/', views.logout_view, name='logout'),
-    path('password-reset/', views.password_reset_view, name='password_reset'),
-    path('check-email/', views.check_email_api, name='check_email_api'),
 ]
